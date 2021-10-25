7db969885d3783c41a5e76b94370397daf584ad6
GOJABoop <josealbertogarcia453@gmail.com>
2021-10-24 22:52:27 -0500

    Creating project

---
+++ PointOfSale/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alberto
+ * Date: 24/10/2021
+ * Time: 10:46 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PointOfSale
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
